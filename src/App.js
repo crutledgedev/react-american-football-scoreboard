4e601f76b345f722effbc27f22a1ccfcc49a4199
Chance Rutledge <chancerutledge2@gmail.com>
2020-05-23 08:57:52 -0700

    component breakouts and ternary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import "./App.css";
-// import BottomRow from "./BottomRow";
+import BottomRow from "./BottomRow";
+import TopRow from "./TopRow";
 
 function App() {
   const [homeCount, setHomeCount] = useState(0);
@@ -48,35 +49,12 @@ function App() {
   return (
     <div className="container">
       <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-            <div className="home__score">{homeCount}</div>
-          </div>
-          <div className="timer">15:00</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayCount}</div>
-          </div>
-        </div>
-        <div className="bottomRow">
-          <div className="down">
-            <h3 className="down__title">Down</h3>
-            <div className="down__value">{currentDown}</div>
-          </div>
-          <div className="toGo">
-            <h3 className="toGo__title">To Go</h3>
-            <div className="toGo__value">{yards2go}</div>
-          </div>
-          <div className="ballOn">
-            <h3 className="ballOn__title">1st on</h3>
-            <div className="ballOn__value">21</div>
-          </div>
-          <div className="quarter">
-            <h3 className="quarter__title">Quarter</h3>
-            <div className="quarter__value">{currentQuarter}</div>
-          </div>
-        </div>
+        <TopRow homeCount={homeCount} awayCount={awayCount} />
+        <BottomRow
+          currentDown={currentDown}
+          yards2go={yards2go}
+          currentQuarter={currentQuarter}
+        />
       </section>
       <section className="buttons">
         <div>
